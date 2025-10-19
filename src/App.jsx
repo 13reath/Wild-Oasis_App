c902a4014a2cfa03ac372ab52d6aa60a3cf96475
13reath <13reathit@gmail.com>
2025-10-19 11:34:41 +0200

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import GlobalStyles from './styles/GlobalStyles.js';
+import AppLayout from './ui/AppLayout.jsx'; 
 import Dashboard from './pages/Dashboard.jsx';
 import Bookings from './pages/Bookings.jsx';
 import Cabins from './pages/Cabins.jsx';
@@ -16,13 +17,16 @@ export default function App() {
             <GlobalStyles />
             <BrowserRouter>
                 <Routes>
-                    <Route index element={<Navigate replace to="dashboard" />} />
-                    <Route path="dashboard" element={<Dashboard />} />
-                    <Route path="bookings" element={<Bookings />} />
-                    <Route path="cabins" element={<Cabins />} />
-                    <Route path="users" element={<Users />} />
-                    <Route path="settings" element={<Settings />} />
-                    <Route path="account" element={<Account />} />
+                    <Route element={<AppLayout />}>
+                        <Route index element={<Navigate replace to="dashboard" />} />
+                        <Route path="dashboard" element={<Dashboard />} />
+                        <Route path="bookings" element={<Bookings />} />
+                        <Route path="cabins" element={<Cabins />} />
+                        <Route path="users" element={<Users />} />
+                        <Route path="settings" element={<Settings />} />
+                        <Route path="account" element={<Account />} />
+                    </Route>
+
                     <Route path="login" element={<Login />} />
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
